a139139b0542dc669064c21f93bd886ac79020d4
paige <paigely@tuta.io>
2026-04-14 18:33:38 +0200

    bugger

--- astro.config.ts
+++ astro.config.ts
@@ -3,21 +3,19 @@ import { defineConfig, fontProviders } from "astro/config";
 export default defineConfig({
 	site: "https://navic.app",
 	trailingSlash: "ignore",
-	experimental: {
-		fonts: [
-			{
-				provider: fontProviders.local(),
-				name: "Google Sans Flex",
-				cssVariable: "--font-gsans",
-				options: {
-					variants: [
-						{
-							weight: "100 900",
-							src: ["./src/assets/fonts/google-sans-flex.ttf"],
-						},
-					],
-				},
+	fonts: [
+		{
+			provider: fontProviders.local(),
+			name: "Google Sans Flex",
+			cssVariable: "--font-gsans",
+			options: {
+				variants: [
+					{
+						weight: "100 900",
+						src: ["./src/assets/fonts/google-sans-flex.ttf"],
+					},
+				],
 			},
-		],
-	},
+		},
+	],
 });

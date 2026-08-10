a9dea015a302a6afc43f1dc0758ef3907ce7bb50
paige <paigely@tuta.io>
2026-08-11 01:59:09 +0200

    feat: eslint

---
+++ eslint.config.ts
@@ -0,0 +1,45 @@
+import js from "@eslint/js"
+import stylistic from "@stylistic/eslint-plugin"
+import { defineConfig, globalIgnores } from "eslint/config"
+import astro from "eslint-plugin-astro"
+import promise from "eslint-plugin-promise"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+
+export default defineConfig([
+	globalIgnores(["dist/", ".astro/", "node_modules/"]),
+
+	js.configs.recommended,
+	tseslint.configs.recommended,
+	astro.configs.recommended,
+	promise.configs["flat/recommended"],
+
+	stylistic.configs.customize({
+		indent: "tab",
+		quotes: "double",
+		semi: false,
+		braceStyle: "1tbs",
+		commaDangle: "never"
+	}),
+
+	{
+		plugins: { "@stylistic": stylistic },
+		rules: {
+			"@stylistic/linebreak-style": ["error", "unix"],
+			"@stylistic/eol-last": ["error", "always"],
+			"@stylistic/max-len": ["error", { code: 100, ignoreStrings: true }],
+			// this rule doesn't like <style>s in astro files if u dont do this
+			"@stylistic/jsx-one-expression-per-line": ["error", { allow: "non-jsx" }]
+		}
+	},
+
+	{
+		languageOptions: { globals: globals.browser },
+		plugins: { "simple-import-sort": simpleImportSort },
+		rules: {
+			"simple-import-sort/imports": "error",
+			"simple-import-sort/exports": "error"
+		}
+	}
+])
